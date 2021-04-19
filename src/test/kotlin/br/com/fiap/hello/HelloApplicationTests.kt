a57b89a519e95b6190e5f955d751b10798c0245a
rayner.lima.vortigo <rayner.lima.vortigo@gruposuperdigital.com>
2021-04-19 16:47:52 -0300

    initial commit

---
+++ src/test/kotlin/br/com/fiap/hello/HelloApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.hello
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
